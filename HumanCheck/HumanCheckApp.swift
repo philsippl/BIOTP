3d42096b37ae39d67a8b6aa669eb0ccd84ed5bdc
Philipp Sippl <philsippl@gmail.com>
2026-03-24 10:47:14 +0100

    Initial Commit

---
+++ HumanCheck/HumanCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumanCheckApp.swift
+//  HumanCheck
+//
+//  Created by ps on 24.03.26.
+//
+
+import SwiftUI
+
+@main
+struct HumanCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HumanCheckTests/HumanCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  HumanCheckTests.swift
+//  HumanCheckTests
+//
+//  Created by ps on 24.03.26.
+//
+
+import Testing
+@testable import HumanCheck
+
+struct HumanCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
